fbaf7e78b344e3c9ac2cb27a04c93f52bc367685
user <mutukukennedy34@gmail.com>
2023-06-14 20:21:00 +0300

    function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun( ){
+    let rl = 'roller - skate!'
+    return "This Saturday, I want to" + rl+"!";
+}
+function mondayWork(){
+    let go = 'got to the office'
+    return "This Monday, I will" + go + "."
+}
+function wrapAdjective(){
+    let result = '*'
+    let emphatic = result ('a hard worker')
+    return 'You are' + result + emphatic + '!'
+}
+
